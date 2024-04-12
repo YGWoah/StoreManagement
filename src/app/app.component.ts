8611e7e0a9c573d7d4406341de87ff958dd7086d
Hamza El Massoudi <hmz.elm@outlook.sa>
2024-04-12 16:25:11 +0000

    Add new components and update dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, RouterModule],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'bankingApp';
+	title = 'Banking App';
+	actions: Array<any> = [
+		{
+			title: 'Home',
+			route: '/home',
+			icon: 'home',
+		},
+		{ title: 'Products', route: '/products', icon: 'list' },
+		{ title: 'New Product', route: '/new-product', icon: 'add' },
+	];
 }
